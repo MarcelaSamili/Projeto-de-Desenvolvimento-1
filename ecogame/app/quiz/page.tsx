3a97e6ce5418fe7ef7ac6cf21ce57562c87eef4b
Marcela Samili <85258527+MarcelaSamili@users.noreply.github.com>
2026-05-16 18:50:13 -0300

    Gamificando

--- ecogame/app/quiz/page.tsx
+++ ecogame/app/quiz/page.tsx
@@ -9,6 +9,7 @@ import { shuffleArray } from '@/utils/shuffle';
 import { useEffect } from 'react';
 import { Question } from '@/types/Quetions';
 import { useSearchParams } from 'next/navigation';
+import { useRef } from 'react';
 //import { getQuestions } from "@/features/quiz/getQuestions";
 export default function Quiz() {
   const searchParams = useSearchParams();
@@ -20,14 +21,45 @@ export default function Quiz() {
   const [respostaSelecionada, setRespostaSelecionada] = useState<string | null>(
     null
   );
+
   const [acertos, setAcertos] = useState(0);
   const [finalizado, setFinalizado] = useState(false);
   const [feedback, setFeedback] = useState('');
   const [perguntas, setPerguntas] = useState<Question[]>([]);
+  const [vidas, setVidas] = useState(3);
+  const [gameOver, setGameOver] = useState(false);
+  const [tempo, setTempo] = useState(15);
+  const [streak, setStreak] = useState(0);
 
   const router = useRouter();
 
   const perguntaAtual = perguntas[index];
+
+  const correctSound = useRef<HTMLAudioElement | null>(null);
+  const wrongSound = useRef<HTMLAudioElement | null>(null);
+  const timeoutSound = useRef<HTMLAudioElement | null>(null);
+  const finishSound = useRef<HTMLAudioElement | null>(null);
+
+  useEffect(() => {
+    correctSound.current = new Audio(
+      '/sounds/mixkit-winning-a-coin-video-game-2069.wav'
+    );
+    wrongSound.current = new Audio(
+      '/sounds/mixkit-player-losing-or-failing-2042.wav'
+    );
+    timeoutSound.current = new Audio('/sounds/mixkit-game-level-music-689.wav');
+    finishSound.current = new Audio(
+      '/sounds/mixkit-completion-of-a-level-2063'
+    );
+    // VOLUME
+    if (correctSound.current) correctSound.current.volume = 0.2;
+
+    if (wrongSound.current) wrongSound.current.volume = 0.2;
+
+    if (timeoutSound.current) timeoutSound.current.volume = 0.2;
+
+    if (finishSound.current) finishSound.current.volume = 0.5;
+  }, []);
 
   {
     /* substitua o useEffect atual caso queira puxar as perguntas do firebase
@@ -58,6 +90,42 @@ export default function Quiz() {
 }, [categoria, dificuldade]);*/
   }
 
+  //SISTEMA DE TEMPO + STREAK
+  useEffect(() => {
+    if (respostaSelecionada || finalizado || gameOver) return;
+    // Toca o som quando o tempo estiver menor ou igual a 14
+    if (tempo >= 1) {
+      timeoutSound.current?.play();
+    } else if (tempo <= 0) {
+      timeoutSound.current?.pause();
+    }
+    // Ação quando o tempo se esgota
+    if (tempo <= 0) {
+      setFeedback('Tempo esgotado!');
+      setVidas(prev => prev - 1);
+      setStreak(0); // PARA O ETREAK VOLTAR A 0 CASO O TEMPO SE ESGOTE
+
+      setTimeout(() => {
+        proximaPergunta();
+      }, 1500);
+      return;
+    }
+    // Decrementa o tempo a cada 1 segundo
+    const timer = setInterval(() => {
+      setTempo(prev => prev - 1);
+    }, 1000);
+    // Limpeza do intervalo para evitar duplicação ou vazamento de memória
+    return () => clearInterval(timer);
+  }, [tempo, respostaSelecionada, finalizado, gameOver]);
+
+  //SISTEMA DE VIDAS
+  useEffect(() => {
+    if (vidas <= 0) {
+      setGameOver(true);
+    }
+  }, [vidas]);
+
+  //SISTEMA DE DIFICULDADE DAS PERGUNTAS
   useEffect(() => {
     if (!categoria || !dificuldade) return;
 
@@ -81,19 +149,31 @@ export default function Quiz() {
     return <p className="text-white p-6">Carregando...</p>;
   }
 
+  //SISTEMA DE SELECAO DE PERGUNTAS
   function selecionarResposta(opcao: string) {
+    setTempo(15);
+    setFeedback('');
     if (respostaSelecionada) return;
 
     setRespostaSelecionada(opcao);
 
     if (opcao === perguntaAtual.resposta) {
-      setAcertos(acertos + 1);
+      correctSound.current?.play();
+      setAcertos(prev => prev + 1);
+
+      setStreak(prev => prev + 1);
+
       setFeedback('✅ Resposta correta!');
     } else {
       setFeedback('❌ Resposta incorreta.');
+      wrongSound.current?.play();
+
+      setVidas(prev => prev - 1);
+      setStreak(0);
     }
   }
 
+  //SISTEMA DE MUDANCA DE PERGUNTAS
   async function proximaPergunta() {
     if (index + 1 < perguntas.length) {
       setIndex(index + 1);
@@ -101,14 +181,34 @@ export default function Quiz() {
       setFeedback('');
     } else {
       setFinalizado(true);
+      finishSound.current?.play();
 
       // cálculo de XP
-      const xp = acertos * 10;
+      const bonus = streak >= 3 ? streak * 5 : 0;
+
+      const xp = acertos * 10 + bonus;
 
       if (auth.currentUser) {
         await addXP(auth.currentUser.uid, xp);
       }
     }
+  }
+
+  if (gameOver) {
+    return (
+      <main className="min-h-screen bg-slate-900 text-white flex flex-col items-center justify-center">
+        <h1 className="text-4xl mb-4">💀 Game Over</h1>
+
+        <p className="mb-6">Você ficou sem vidas!</p>
+
+        <button
+          onClick={() => router.push('/dashboard')}
+          className="bg-red-500 px-4 py-2 rounded"
+        >
+          Voltar ao Dashboard
+        </button>
+      </main>
+    );
   }
 
   if (finalizado) {
@@ -132,6 +232,21 @@ export default function Quiz() {
 
   return (
     <main className="min-h-screen bg-slate-900 text-white p-6">
+      {/* VIDAS NA TELA */}
+      <div className="flex gap-2 mb-4 text-2xl">
+        {[...Array(vidas)].map((_, i) => (
+          <span key={i}>❤️</span>
+        ))}
+      </div>
+      {/* STREAKS */}
+      <div className="mb-4 text-xl">
+        {streak >= 3 ? (
+          <p className="text-yellow-400 font-bold">🔥🔥🔥 COMBO x{streak}</p>
+        ) : (
+          <p>🔥 Streak: {streak}</p>
+        )}
+      </div>
+
       {/* PROGRESSO */}
       <div className="mb-6">
         <p className="mb-2">
@@ -151,6 +266,18 @@ export default function Quiz() {
       <p className="text-sm text-slate-400 mb-2">
         {perguntaAtual.categoria} • {perguntaAtual.dificuldade}
       </p>
+      {/* TIMER*/}
+      <div className="mb-4">
+        <p className="text-lg">⏱️ Tempo restante: {tempo}s</p>
+      </div>
+      <div className="w-full bg-slate-700 h-2 rounded mb-6">
+        <div
+          className="bg-yellow-400 h-2 rounded transition-all"
+          style={{
+            width: `${(tempo / 15) * 100}%`,
+          }}
+        />
+      </div>
       {/* PERGUNTA */}
       <h1 className="text-xl mb-6">{perguntaAtual.pergunta}</h1>
 
--- ecogame/data/questions.ts
+++ ecogame/data/questions.ts
@@ -1,36 +1,67 @@
 import { Question } from '@/types/Quetions';
 
 export const questions: Question[] = [
+  //Reciclagem
+
   {
     id: '1',
-    pergunta: 'Qual destes materiais pode ser reciclado?',
-    opcoes: ['Espelho', 'Plástico', 'Guardanapo sujo', 'Papel engordurado'],
-    resposta: 'Plástico',
-    explicacao: 'O plástico pode ser reciclado.',
+    pergunta: 'Qual dos biomas abaixo não fica no Brasil?',
+    opcoes: ['Caatinga', ' Mata Atlântica', 'Tundra', 'Pantanal'],
+    resposta: 'Tundra',
+    explicacao: '',
     categoria: 'reciclagem',
     dificuldade: 'easy',
   },
   {
     id: '2',
-    pergunta: 'Qual fonte de energia é renovável?',
-    opcoes: ['Carvão', 'Petróleo', 'Solar', 'Gás natural'],
-    resposta: 'Solar',
-    explicacao: 'Energia solar é limpa e renovável.',
-    categoria: 'energia',
-    dificuldade: 'medium',
+    pergunta: ' Como preservar árvores e florestas?',
+    opcoes: [
+      'Construindo uma casa na árvore',
+      'Reciclando papéis, jornais e revistas',
+      'Reutilizando metais e vidros',
+      'Indo a parques',
+    ],
+    resposta: 'Construindo uma casa na árvore',
+    explicacao: '',
+    categoria: 'reciclagem',
+    dificuldade: 'easy',
   },
   {
     id: '3',
-    pergunta: 'O que é sustentabilidade?',
-    opcoes: [
-      'Consumir mais rápido',
-      'Preservar recursos para o futuro',
-      'Evitar tecnologia',
-      'Usar tudo sem controle',
-    ],
-    resposta: 'Preservar recursos para o futuro',
-    explicacao: 'Sustentabilidade garante o futuro.',
+    pergunta: ' Ao solicitar um delivery, qual a embalagem mais sustentável?',
+    opcoes: ['De plástico', 'De papel reciclado'],
+    resposta: 'De papel reciclado',
+    explicacao: '',
     categoria: 'sustentabilidade',
+    dificuldade: 'easy',
+  },
+  {
+    id: '4',
+    pergunta: ' Como separar corretamente seu lixo?',
+    opcoes: [
+      'Juntar tudo na lixeira, pois os prédios já fazem o trabalho de separação.',
+      'Separar o lixo orgânico (restos de alimentos, papel sujo e lixo sanitário) dos resíduos sólidos (como plástico, vidro, papel, metal e embalagens longa vida).',
+      'Deixar plásticos sujos junto com lixo orgânico.',
+      'Juntar todo tipo de lixo e descartar em ponto de coleta.',
+    ],
+    resposta:
+      'Separar o lixo orgânico (restos de alimentos, papel sujo e lixo sanitário) dos resíduos sólidos (como plástico, vidro, papel, metal e embalagens longa vida).',
+    explicacao: '',
+    categoria: 'reciclagem',
+    dificuldade: 'easy',
+  },
+  {
+    id: '5',
+    pergunta: 'Os lixos coletados não podem ser levados para:',
+    opcoes: [
+      'Aterros sanitários',
+      'Empresa recicladora',
+      'Terrenos baldios',
+      'Todas alternativa acima',
+    ],
+    resposta: 'Terrenos baldios',
+    explicacao: '',
+    categoria: 'reciclagem',
     dificuldade: 'easy',
   },
 ];
